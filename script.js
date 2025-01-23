1548e6ed4d4fa448e3565d6b66de9d72301f2f8d
Loknath P <146057538+loknath0711@users.noreply.github.com>
2025-01-23 20:25:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer . . ." , "UI/UX Designer . . ." , "Software Developer . . .","Full Stack Developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
