189d43bc44ee7cdfd29e8038aeb4e2a8c51c4724
Loknath P <146057538+loknath0711@users.noreply.github.com>
2025-01-23 20:23:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Frontend Developer . . ." , "UI/UX Designer . . ." , "Software Developer . . .","Full Stack Developer . . ."],
-    typeSpeed: 70,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
-})
